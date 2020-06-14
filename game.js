2632666c814fdfe280d5eee57e35f4f20f7af612
Naman Oli <53824950+NamanOli@users.noreply.github.com>
2020-06-14 19:41:39 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,13 @@
 function load_images(){
     //player,virus,gem
     enemy_image = new Image;
-    enemy_image.src = "v1.png";
+    enemy_image.src = "v2.png";
     
     player_img = new Image;
     player_img.src = "superhero.png";
     
     gem_image = new Image;
-    gem_image.src = "gemm.png";
+    gem_image.src = "gem.png";
     
     
 }
